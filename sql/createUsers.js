939bd4a58ed2e45f2e0c2ace96d24c045b9a6b37
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2021-11-03 12:04:23 +0000

    Set default password and remove manual passwords

--- sql/createUsers.js
+++ sql/createUsers.js
@@ -6,11 +6,11 @@
 const createUsers =`
 CREATE TABLE IF NOT EXISTS Users (
     userid INTEGER PRIMARY KEY NOT NULL,
-    friendlyname VARCHAR(50),  
+    friendlyname VARCHAR(50),
     emailaddress VARCHAR(230) UNIQUE,
-    password VARCHAR(256), -- password and admin are SQL reserved words, need to ask if we should change these to non reserved
+    password VARCHAR(256) DEFAULT "--blank--", -- password and admin are SQL reserved words, need to ask if we should change these to non reserved
     admin INTEGER(1) NOT NULL,
     lastlogin INTEGER DEFAULT (strftime('%s','now'))
-);
+  );
 `
 module.exports = { createUsers };
--- sql/insertUsers.js
+++ sql/insertUsers.js
@@ -6,22 +6,22 @@
 // the last login timestamp as 0 (0 = Unix Epoch i.e., Jan 01 1970) to indicate that no previous login attempts have been recorded.
 
 const insertUsers = `
-INSERT INTO Users (userid, friendlyname, emailaddress, password, admin, lastlogin) VALUES 
-    (1000, "Abraham Lincoln", "abraham@ada.ac.uk", "--blank--", 1, 0),
-    (1001, "Anne Frank", "anne@ada.ac.uk", "--blank--", 0, 0),
-    (1002, "Aristotle", "aristotle@ada.ac.uk", "--blank--", 0, 0),
-    (1003, "Benjamin Franklin", "benjamin@ada.ac.uk", "--blank--", 0, 0),
-    (1004, "Conficus", "conficus@ada.ac.uk", "--blank--", 0, 0),
-    (1005, "Eleanor Roosevelt", "eleanor@ada.ac.uk", "--blank--", 0, 0),
-    (1006, "Franklin D. Roosevelt", "franklin@ada.ac.uk", "--blank--", 0, 0),
-    (1007, "Kamala Harris", "kamala@ada.ac.uk", "--blank--", 1, 0),
-    (1008, "Mark Twain", "mark@ada.ac.uk", "--blank--", 0, 0), 
-    (1009, "Oprah Winfrey", "oprah@ada.ac.uk", "--blank--", 1, 0),
-    (1010, "Socrates", "socrates@ada.ac.uk", "--blank--", 0, 0),
-    (1011, "Steve Jobs", "steve@ada.ac.uk", "--blank--", 1, 0),
-    (1012, "Thomas A. Eddison", "tom@ada.ac.uk", "--blank--", 0, 0),
-    (1013, "Thomas Jefferson", "thomas@ada.ac.uk", "--blank--", 0, 0),
-    (1014, "Alexei Navalny", "alexei@ada.ac.uk", "--blank--", 1, 0)
+INSERT INTO Users (userid, friendlyname, emailaddress, admin, lastlogin) VALUES 
+    (1000, "Abraham Lincoln", "abraham@ada.ac.uk", 1, 0),
+    (1001, "Anne Frank", "anne@ada.ac.uk", 0, 0),
+    (1002, "Aristotle", "aristotle@ada.ac.uk", 0, 0),
+    (1003, "Benjamin Franklin", "benjamin@ada.ac.uk", 0, 0),
+    (1004, "Conficus", "conficus@ada.ac.uk", 0, 0),
+    (1005, "Eleanor Roosevelt", "eleanor@ada.ac.uk", 0, 0),
+    (1006, "Franklin D. Roosevelt", "franklin@ada.ac.uk", 0, 0),
+    (1007, "Kamala Harris", "kamala@ada.ac.uk", 1, 0),
+    (1008, "Mark Twain", "mark@ada.ac.uk", 0, 0), 
+    (1009, "Oprah Winfrey", "oprah@ada.ac.uk", 1, 0),
+    (1010, "Socrates", "socrates@ada.ac.uk", 0, 0),
+    (1011, "Steve Jobs", "steve@ada.ac.uk", 1, 0),
+    (1012, "Thomas A. Eddison", "tom@ada.ac.uk", 0, 0),
+    (1013, "Thomas Jefferson", "thomas@ada.ac.uk", 0, 0),
+    (1014, "Alexei Navalny", "alexei@ada.ac.uk", 1, 0)
 ;
 `
 module.exports = { insertUsers };
